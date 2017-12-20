f1a8c8b74dfd1efe51695beb35fb7c47e3d75196
dakotalong1996 <longd14@students.ecu.edu>
2017-12-19 21:11:13 -0500

    Edited terrain, added event triggers and sounds
    Added more detail to the terain including houses, lamps, and hallways.  Added fog and more textures.  Triggers an event to spawn a scarecrow and make a scary noise.  Added barriers to prevent user from leaving map.

---
+++ Assets/MoveScarecrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScarecrow : MonoBehaviour {
+
+	private GameObject scarecrow;
+	// Use this for initialization
+	void OnTriggerEnter(Collider col)
+	{
+		if (this.enabled) 
+		{
+			this.enabled = false;
+			GameObject scarecrow = GameObject.Find ("Scarecrow1");
+			Debug.Log ("Object entered the trigger.");
+			scarecrow.transform.position = new Vector3 ((float)418.209, (float)0.12, (float)417.428);
+			scarecrow.GetComponent<AudioSource> ().Play ();
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		GameObject scarecrow = GameObject.Find ("Scarecrow1");
+		var destroyTimer = 4.5;
+		Destroy (scarecrow, (float) destroyTimer);
+	}
+}
---
+++ Assets/StartAnimScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimScript : MonoBehaviour {
+
+	private Animator anim;
+	private GameObject crawler;
+	private bool ready;
+	private float speed;
+	private float distance;
+	private float xStartPosition;
+
+
+	void Start () {
+		xStartPosition = transform.position.x;
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("Entered second trigger.");
+		if (this.enabled) 
+		{
+			GameObject crawler = GameObject.Find ("crawler");
+			anim = crawler.GetComponent<Animator> ();
+			ready = true;
+			this.enabled = false;
+		}
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (ready == true)
+		{
+			speed *= -1;
+			crawler.transform.position = new Vector3 (transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+
+	}
+}
